534c1d5d0b08d5c4a852b719b86c64ec98c578cf
Tiffany Lam <tiffanylamhl@gmail.com>
2015-08-05 23:00:14 -0700

    Refined add functions, no empty tags can be added

--- src/ui/CourseScheduler/WebPageJS.js
+++ src/ui/CourseScheduler/WebPageJS.js
@@ -13,10 +13,25 @@ main = function () {
                 $(this).parent().remove();
             });
         $("<li>").text(tag).draggable().append(deleteButton).appendTo(".course-tags");
+        $("#coursecode").val("");
+        $(".btn").addClass("disabled");
     });
 
+    $(".input-box").keyup(function(){
+        var codeLength = $(this).val().length;
+        var remainChars=12 - codeLength;
+        if(remainChars == 12 || remainChars < 0){
+            $(".course-input .btn").addClass("disabled");
+        } else{
+            $(".course-input .btn").removeClass("disabled");
+        }
+    });
 
+    //TODO: add in functionality to serve backend
+    //TODO: check whther course code is valid
+    //TODO: use sweet alert if not
 
+    $(".btn").addClass("disabled");
 }
 
 $(document).ready(main);
