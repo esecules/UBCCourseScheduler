5f47f12bdde472f92ed4c6f8cac8d2262301c927
Tiffany Lam <tiffanylamhl@gmail.com>
2015-08-14 12:57:30 -0700

    Page doesn't scroll to top when 'add' clicked

--- src/ui/CourseScheduler/WebPageJS.js
+++ src/ui/CourseScheduler/WebPageJS.js
@@ -31,20 +31,8 @@ main = function () {
                 editable: true
             });
             $("#coursecode").val("");
-            /*if(tag == "CPSC 304"){
-                var lecture = {
-                    id     :'304',
-                    title  :'CPSC 304 Lecture',
-                    start  :'2015-08-26 09:00:00',
-                    end    :'2015-08-26 11:00:00',
-                    allDay : false,
-
-                    location: 'DMP 310',
-                    editable: true
-                }
-                calendar.fullCalendar('renderEvent', lecture, true)
-            }*/
         }
+        return false;
     });
 
     var calendar = $("#calendar").fullCalendar({
