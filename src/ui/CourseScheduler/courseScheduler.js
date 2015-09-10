cb282c5ba34899bcb94dfad75b26b5564b924461
Tiffany Lam <tiffanylamhl@gmail.com>
2015-09-10 01:09:17 -0700

    Added comments, dsicvered bug on Safari, second section of lecture not showing up when checked(test data)

--- src/ui/CourseScheduler/courseScheduler.js
+++ src/ui/CourseScheduler/courseScheduler.js
@@ -6,6 +6,7 @@ var main;
 //Look for ** //UNCOMMENT THIS FOR AJAX!! ** for all sections to uncomment for ajax to work
 
 main = function () {
+    //Sample JSON data for functions testing
     var course = [
         {
             "hasSection": true,
@@ -264,11 +265,13 @@ main = function () {
         weekends: false,
         theme: false,
         defaultDate: "2015-08-24",
+        //No header display
         header: {
             left: "none",
             center: "none",
             right: "none"
         },
+        //Restrict column format to just showing day of the week
         columnFormat: {
             month: 'ddd',
             week: 'ddd',
@@ -287,11 +290,13 @@ main = function () {
         weekends: false,
         theme: false,
         defaultDate: "2015-08-24",
+        //No header display
         header: {
             left: "none",
             center: "none",
             right: "none"
         },
+        //Restrict column format to just showing day of the week
         columnFormat: {
             month: 'ddd',
             week: 'ddd',
@@ -303,15 +308,18 @@ main = function () {
 
     var activeTab;
 
+    //Initializes first tab to show
     $(function(){
         $("#switchTabs a:first").tab('show');
     });
 
+    //Renders Fullcalendar onto the tab
+    //stores active tab in activeTab
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         $('#calendar').fullCalendar('render');
         $('#calendar2').fullCalendar('render');
         activeTab = $(".tabbable li.active").attr("id");
-        $("#idDisplay").text(activeTab);
+        //$("#idDisplay").text(activeTab);
     });
 
 
@@ -357,11 +365,13 @@ main = function () {
             });
         }
 
-        //valid coursecode  format
+        //valid coursecode format
         else {
 
-
+            //takes user input and get departmental code
             var department = tag.substr(0, 4).toUpperCase();
+
+            //takes user input and get course code
             var code = tag.substr(4).trim().toUpperCase();
 
             //UNCOMMENT THIS FOR AJAX!!
@@ -391,17 +401,22 @@ main = function () {
             var numTutorial2 = course[0].tutorials_term2.length;
             var numLaboratory2 = course[0].labs_term2.length;
 
-
+            //create dropdown menu and returns newly created dropdown menu
             function createDropdownMenu() {
                 return $("<ul>").addClass("dropdown-menu");
             }
 
+            //create a single checkbox and returns newly created checkbox
             function createCheckBox() {
                 return $('<input />', {
                     type: 'checkbox'
                 }).addClass("dd-check-boxes");
             }
 
+            //A hack to put the respective weekly day
+            //to the limited view Fullcalendar for scheduling purposes
+            //function determines the respective date on calendar limited
+            //to the week of 8-24-2015 to 8-28-2015
             function determineDay(day) {
                 switch (day) {
                     case "Mon":
@@ -417,6 +432,8 @@ main = function () {
                 }
             }
 
+            //create an individual Event
+            //puts newly created Event into a given position of a list of weekly Events
             function eventCreate(listOfEvents, current, indJ) {
                 listOfEvents[indJ] = {
                     id: current.section,
@@ -428,6 +445,8 @@ main = function () {
                 };
             }
 
+            //creates a list of weekly recurring Events for a specified lecture/tutorial/lab section
+            //returns newly created weekly recurring Events
             function iterateCourse(index, identifier, term) {
                 var array = [];
                 if(term == 1){
@@ -467,11 +486,15 @@ main = function () {
                 }
             }
 
+            //creates an item of the dropdown menu
+            //item is an Event object (lecture/lab/tutorial happens onece/week) or
+            //a list of Event objects (lecture/lab/tutorial happens more than onece/week)
             function createItem(checkBox, tag, index, identifier, term) {
                 return $("<li>").text(tag).addClass("dd-options").append(checkBox)
                     .data("event", iterateCourse(index, identifier, term));
             }
 
+            //function fills up the dropdown menu with items
             function populateDropdownMenu(num, ddMenu, ddCB, ddItems, tags, identifier, term) {
                 for (var i = 0; i < num ; i++) {
                     ddCB[i] = createCheckBox();
@@ -480,6 +503,7 @@ main = function () {
                 }
             }
 
+            //creates a delete button and returns newly created delete button
             function createDeleteButton() {
                 return $("<button>").button({
                     icons: {primary: "ui-icon-close"},
@@ -488,6 +512,9 @@ main = function () {
                 }).addClass("dd-delete-buttons");
             }
 
+            //creates a delete button, attaches toggle button with a click event to a give dropdown menu
+            //toggle button toggles dropdown menu
+            //returns newly created delete button
             function createToggleButton(ddMenu) {
                 return $("<a>").button({
                     icons: {primary: "ui-icon-triangle-1-s"},
@@ -497,6 +524,8 @@ main = function () {
                 });
             }
 
+            //creates a course tag with given tag name, dropdown menu, toggle button, delete button
+            //and term
             function createTag(tagName, ddMenu, toggleButton, deleteButton, term) {
                 if(term == 1){
                     $("<li>").text(tagName).draggable({
@@ -515,6 +544,15 @@ main = function () {
                 }
             }
 
+            //Legend:
+            //sectionCB == a list of lecture section check boxes
+            //sectionitems == a list of lecture events under the same lecture id
+            //sectionDD == lecture section dropdown menu
+            //sectionDB == delete button for lecture tag
+            //sectionTB == toggle button for lecture tag's dropdown menu
+            //...
+
+            //if term1 calendar is the active tab shown
             if(activeTab == "term1"){
                 alert("TERM 1!!!!!");
                 if (course[0].hasSection == true) {
@@ -546,39 +584,42 @@ main = function () {
                     var laboratoryTB = createToggleButton(laboratoryDD);
                     createTag(department + code + " Lab", laboratoryDD, laboratoryTB, laboratoryDB, 1);
                 }
-            }else if(activeTab == "term2"){
+            }
+            //if term2 calendar is the active tab shown
+            else if(activeTab == "term2"){
                 alert("TERM 2!!!!!");
                 if (course[0].hasSection == true) {
-                    var sectionCB = [];
-                    var sectionItems = [];
-                    var sectionDD = createDropdownMenu();
-                    populateDropdownMenu(numSection2,sectionDD, sectionCB, sectionItems, course[0].sections2, "lecture", 2);
-                    var sectionDB = createDeleteButton();
-                    var sectionTB = createToggleButton(sectionDD);
-                    createTag(department + code + " Lec", sectionDD, sectionTB, sectionDB, 2);
+                    var sectionCB2 = [];
+                    var sectionItems2 = [];
+                    var sectionDD2 = createDropdownMenu();
+                    populateDropdownMenu(numSection2,sectionDD2, sectionCB2, sectionItems2, course[0].sections2, "lecture", 2);
+                    var sectionDB2 = createDeleteButton();
+                    var sectionTB2 = createToggleButton(sectionDD2);
+                    createTag(department + code + " Lec", sectionDD2, sectionTB2, sectionDB2, 2);
                 }
 
                 if (course[0].hasTutorial == true) {
-                    var tutorialCB = [];
-                    var tutorialItems = [];
-                    var tutorialDD = createDropdownMenu();
-                    populateDropdownMenu(numTutorial2,tutorialDD, tutorialCB, tutorialItems, course[0].tutorials2, "tutorial", 2);
-                    var tutorialDB = createDeleteButton();
-                    var tutorialTB = createToggleButton(tutorialDD);
-                    createTag(department + code + " Tut", tutorialDD, tutorialTB, tutorialDB, 2);
+                    var tutorialCB2 = [];
+                    var tutorialItems2 = [];
+                    var tutorialDD2 = createDropdownMenu();
+                    populateDropdownMenu(numTutorial2,tutorialDD2, tutorialCB2, tutorialItems2, course[0].tutorials2, "tutorial", 2);
+                    var tutorialDB2 = createDeleteButton();
+                    var tutorialTB2 = createToggleButton(tutorialDD2);
+                    createTag(department + code + " Tut", tutorialDD2, tutorialTB2, tutorialDB2, 2);
                 }
 
                 if (course[0].hasLaboratory == true) {
-                    var laboratoryCB = [];
-                    var laboratoryItems = [];
-                    var laboratoryDD = createDropdownMenu();
-                    populateDropdownMenu(numLaboratory2,laboratoryDD, laboratoryCB, laboratoryItems, course[0].labs2, "laboratory", 2);
-                    var laboratoryDB = createDeleteButton();
-                    var laboratoryTB = createToggleButton(laboratoryDD);
-                    createTag(department + code + " Lab", laboratoryDD, laboratoryTB, laboratoryDB, 2);
+                    var laboratoryCB2 = [];
+                    var laboratoryItems2 = [];
+                    var laboratoryDD2 = createDropdownMenu();
+                    populateDropdownMenu(numLaboratory2,laboratoryDD2, laboratoryCB2, laboratoryItems2, course[0].labs2, "laboratory", 2);
+                    var laboratoryDB2 = createDeleteButton();
+                    var laboratoryTB2 = createToggleButton(laboratoryDD2);
+                    createTag(department + code + " Lab", laboratoryDD2, laboratoryTB2, laboratoryDB2, 2);
                 }
             }
 
+            //Clears input box for next user input
             $("#coursecode").val("");
         }
         //} //UNCOMMENT THIS FOR AJAX!!
@@ -586,6 +627,8 @@ main = function () {
         return false;
     });
 
+    //Iteratively renders a list of events onto either term1 or term2 calendar,
+    //according to identifier
     function eventsRender(events, identifier) {
         if(activeTab == "term1"){
             for (var i = 0; i < events.length; i++) {
@@ -600,6 +643,10 @@ main = function () {
 
 
     $(".course-tags")
+        //click event when delete button on course tag is clicked
+        //finds parent - the li element itself
+        //and iteratively delete all child elements
+        //remove all events on calendar with the same id as parent
         .on("click", ".dd-delete-buttons", function () {
             $(this).parent().find("li").each(function(index){
                 if(activeTab == "term1"){
@@ -611,6 +658,8 @@ main = function () {
             $(this).closest(".course-tag").remove();
             return false;
         })
+        //Renders event on calendar when item is checked, add class checked-box
+        //Remove item when item is unchecked, removes class checked-box
         .on("click", ".dd-check-boxes", function () {
             if ($(".dd-check-boxes").is(":checked") && !($(this).hasClass("checked-box"))) {
                 $(this).addClass("checked-box");
@@ -628,7 +677,7 @@ main = function () {
 
 
     //TEST: manually added event object
-    var cpsc310 = {
+    /*var cpsc310 = {
         id: '310',
         title: 'CPSC 310 Lecture',
         start: '2015-08-24 09:00:00',
@@ -651,8 +700,8 @@ main = function () {
     };
 
     calendar_term1.fullCalendar('renderEvent', cpsc310, true);
-    calendar_term2.fullCalendar('renderEvent', cpsc313, true);
+    calendar_term2.fullCalendar('renderEvent', cpsc313, true);*/
 };
 
-//Function execute when HTML doc is loaded
+//Function executes when HTML doc is loaded
 $(document).ready(main);
